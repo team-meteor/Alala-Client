5e88fc334392cb6187d40cc814bf3adc4c771043
hoemoon <hoemoon.song@gmail.com>
2017-06-12 17:50:45 +0900

    S3 upload test

--- Alala/FeedViewController.swift
+++ Alala/FeedViewController.swift
@@ -6,43 +6,44 @@
 //  Copyright © 2017 team-meteor. All rights reserved.
 //
 import UIKit
+import Alamofire
 
 class FeedViewController: UIViewController {
-    
-    fileprivate let cameraButton = UIBarButtonItem(
-        image: UIImage(named: "camera")?.resizeImage(scaledTolength: 25),
-        style: .plain,
-        target: nil,
-        action: nil
-    )
-    
-    fileprivate let messageButton = UIBarButtonItem(
-        image: UIImage(named: "message")?.resizeImage(scaledTolength: 25),
-        style: .plain,
-        target: nil,
-        action: nil
-    )
-    
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        self.tabBarItem.image = UIImage(named: "feed")?.resizeImage(scaledTolength: 25)
-        self.tabBarItem.selectedImage = UIImage(named: "feed-selected")?.resizeImage(scaledTolength: 25)
-        self.tabBarItem.imageInsets.top = 5
-        self.tabBarItem.imageInsets.bottom = -5
-        self.navigationItem.leftBarButtonItem = cameraButton
-        self.navigationItem.rightBarButtonItem = messageButton
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.navigationItem.titleView = UILabel().then {
-            $0.font = UIFont(name: "IowanOldStyle-BoldItalic", size: 20)
-            $0.text = "Alala"
-            $0.sizeToFit()
-        }
-    }
+	
+	fileprivate let cameraButton = UIBarButtonItem(
+		image: UIImage(named: "camera")?.resizeImage(scaledTolength: 25),
+		style: .plain,
+		target: nil,
+		action: nil
+	)
+	
+	fileprivate let messageButton = UIBarButtonItem(
+		image: UIImage(named: "message")?.resizeImage(scaledTolength: 25),
+		style: .plain,
+		target: nil,
+		action: nil
+	)
+	
+	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+		self.tabBarItem.image = UIImage(named: "feed")?.resizeImage(scaledTolength: 25)
+		self.tabBarItem.selectedImage = UIImage(named: "feed-selected")?.resizeImage(scaledTolength: 25)
+		self.tabBarItem.imageInsets.top = 5
+		self.tabBarItem.imageInsets.bottom = -5
+		self.navigationItem.leftBarButtonItem = cameraButton
+		self.navigationItem.rightBarButtonItem = messageButton
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.navigationItem.titleView = UILabel().then {
+			$0.font = UIFont(name: "IowanOldStyle-BoldItalic", size: 20)
+			$0.text = "Alala"
+			$0.sizeToFit()
+		}
+	}
 }
