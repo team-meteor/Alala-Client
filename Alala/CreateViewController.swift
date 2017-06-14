c9d82c47cdc0b99db6d912131c8467323d31221e
torpedo87 <bestofhand@naver.com>
2017-06-14 18:52:59 +0900

    setting scrollview layout

--- Alala/CreateViewController.swift
+++ Alala/CreateViewController.swift
@@ -11,9 +11,15 @@ import Photos
 
 class CreateViewController: UIViewController {
     
-    fileprivate let imageView = UIImageView().then {
-        $0.backgroundColor = UIColor.yellow
+    fileprivate let scrollView = UIScrollView().then {
+        $0.alwaysBounceHorizontal = true
+        $0.alwaysBounceVertical = true
+        $0.showsHorizontalScrollIndicator = false
+        $0.showsVerticalScrollIndicator = false
+        $0.maximumZoomScale = 3
     }
+    
+    fileprivate let imageView = UIImageView()
     
     var fetchResult: PHFetchResult<PHAsset>!
     let tileCellSpacing = CGFloat(3)
@@ -23,52 +29,6 @@ class CreateViewController: UIViewController {
         $0.backgroundColor = .white
         $0.alwaysBounceVertical = true
         $0.register(TileCell.self, forCellWithReuseIdentifier: "tileCell")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.collectionView.dataSource = self
-        self.collectionView.delegate = self
-        
-        self.view.addSubview(self.imageView)
-        self.view.addSubview(self.collectionView)
-        
-        //상대적 위치 선정
-        self.imageView.snp.makeConstraints { make in
-            make.top.left.right.equalToSuperview()
-            make.bottom.equalTo(self.collectionView.snp.top)
-        }
-        
-        self.collectionView.snp.makeConstraints { make in
-            make.top.equalTo(self.imageView.snp.bottom)
-            make.left.bottom.right.equalToSuperview()
-        }
-        
-        fetchAllPhotos()
-        
-        //가장 최근 사진 프리뷰
-        let firstAsset = fetchResult.object(at: 0)
-        imageManager.requestImage(for: firstAsset, targetSize: CGSize(width: 640, height: 640), contentMode: .aspectFit, options: nil, resultHandler: { image, _ in
-            self.imageView.image = image
-        })
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        self.initializeContentSizeIfNeeded()
-    }
-    
-    func initializeContentSizeIfNeeded() {
-        imageView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height/2)
-        collectionView.frame = CGRect(x: 0, y: self.view.bounds.midY, width: self.view.bounds.width, height: self.view.bounds.height/2)
-    }
-    
-    func fetchAllPhotos() {
-        let allPhotosOptions = PHFetchOptions()
-        allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-        fetchResult = PHAsset.fetchAssets(with: allPhotosOptions)
     }
     
     init() {
@@ -90,11 +50,73 @@ class CreateViewController: UIViewController {
         
         self.automaticallyAdjustsScrollViewInsets = false
         
+        print("init")
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        scrollView.delegate = self
+        
+        self.scrollView.addSubview(self.imageView)
+        self.view.addSubview(self.scrollView)
+        self.view.addSubview(self.collectionView)
+        
+        self.scrollView.snp.makeConstraints { make in
+            make.left.right.equalTo(self.view)
+            make.height.equalTo(self.view.bounds.width)
+        }
+        
+        self.collectionView.snp.makeConstraints { make in
+            make.left.bottom.right.equalTo(self.view)
+            make.top.equalTo(self.scrollView.snp.bottom)
+        }
+        
+        fetchAllPhotos()
+        let firstAsset = fetchResult.object(at: 0)
+        let scale = UIScreen.main.scale
+        let targetSize = CGSize(width: 600 * scale, height: 600 * scale)
+        imageManager.requestImage(for: firstAsset, targetSize: targetSize, contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
+            let imageWidth = image!.size.width
+            let imageHeight = image!.size.height
+            if imageWidth > imageHeight {
+                self.scrollView.contentSize.height = self.scrollView.frame.height
+                self.scrollView.contentSize.width = imageWidth * self.scrollView.frame.height / imageHeight
+            } else if imageWidth < imageHeight {
+                self.scrollView.contentSize.width = self.scrollView.frame.width
+                self.scrollView.contentSize.height = imageHeight * self.scrollView.frame.width / imageWidth
+            } else {
+                self.scrollView.contentSize.width = self.scrollView.frame.width * 1.2
+                self.scrollView.contentSize.height = self.scrollView.frame.height * 1.2
+            }
+            self.imageView.frame.size = self.scrollView.contentSize
+            self.imageView.center.x = UIScreen.main.bounds.size.width / 2
+            self.imageView.center.y = self.scrollView.center.y
+            self.imageView.image = image
+        })
+        print("viewdid")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.scrollView.snp.makeConstraints { make in
+            make.top.equalTo(self.navigationController!.navigationBar.snp.bottom)
+        }
+        print("layout")
+    }
+
+    func fetchAllPhotos() {
+        let allPhotosOptions = PHFetchOptions()
+        allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        fetchResult = PHAsset.fetchAssets(with: allPhotosOptions)
+    }
+    
+    
     
     func cancelButtonDidTap() {
         self.dismiss(animated: true, completion: nil)
@@ -114,7 +136,9 @@ extension CreateViewController: UICollectionViewDataSource {
         cell.representedAssetIdentifier = asset.localIdentifier
         
         //메타데이터를 이미지로 변환
-        imageManager.requestImage(for: asset, targetSize: CGSize(width: 640, height: 640), contentMode: .aspectFit, options: nil, resultHandler: { image, _ in
+        let scale = UIScreen.main.scale
+        let targetSize = CGSize(width: 600 * scale, height: 600 * scale)
+        imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
             if cell.representedAssetIdentifier == asset.localIdentifier {
                 cell.configure(photo: image!)
             }
@@ -147,8 +171,19 @@ extension CreateViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let asset = fetchResult.object(at: indexPath.item)
-        imageManager.requestImage(for: asset, targetSize: CGSize(width: 640, height: 640), contentMode: .aspectFit, options: nil, resultHandler: { image, _ in
+        let scale = UIScreen.main.scale
+        let targetSize = CGSize(width: 600 * scale, height: 600 * scale)
+        
+        imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
             self.imageView.image = image
         })
     }
+}
+
+extension CreateViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
 }
