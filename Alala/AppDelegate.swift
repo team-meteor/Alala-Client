9ccaf24dc10bfe5b70de57da9862312c5c6e21bc
hoemoon <hoemoon.song@gmail.com>
2017-06-15 19:41:44 +0900

    기본적인 인증 과정 구현 register, login, logout

--- Alala/AppDelegate.swift
+++ Alala/AppDelegate.swift
@@ -8,26 +8,48 @@
 
 import UIKit
 import Then
+import SnapKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
+	var window: UIWindow?
+	static var instance: AppDelegate? {
+		return UIApplication.shared.delegate as? AppDelegate
+	}
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		
+		NotificationCenter.default.addObserver(self, selector: #selector(presentMainTabBarController), name: .presentMainTabBar, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(presentLoginViewController), name: .presentLogin, object: nil)
+		self.configureAppearance()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        self.configureAppearance()
-        // Override point for customization after application launch.
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.backgroundColor = .white
-        window.makeKeyAndVisible()
-        window.rootViewController = MainTabBarController()
-        
-        self.window = window
-        return true
-    }
-    
-    func configureAppearance() {
-        UINavigationBar.appearance().tintColor = .black
-        UIBarButtonItem.appearance().tintColor = .black
-        UITabBar.appearance().tintColor = .black
-    }
+		let window = UIWindow(frame: UIScreen.main.bounds)
+		window.backgroundColor = .white
+		window.makeKeyAndVisible()
+		window.rootViewController = SplashViewController()
+		
+		self.window = window
+		return true
+	}
+	
+	func configureAppearance() {
+		UINavigationBar.appearance().tintColor = .black
+		UIBarButtonItem.appearance().tintColor = .black
+		UITabBar.appearance().tintColor = .black
+	}
+	
+	func presentLoginViewController() {
+		let navigation = UINavigationController(rootViewController: LoginViewController())
+		self.window?.rootViewController = navigation
+	}
+	
+	func presentMainTabBarController() {
+		let tabBarController = MainTabBarController()
+		self.window?.rootViewController = tabBarController
+	}
+}
+
+extension Notification.Name {
+	static var presentMainTabBar: Notification.Name { return .init("presentMainTabBar") }
+	static var presentLogin: Notification.Name { return .init("presentLogin") }
 }
--- Alala/PersonalViewController.swift
+++ Alala/PersonalViewController.swift
@@ -9,23 +9,41 @@
 import UIKit
 
 class PersonalViewController: UIViewController {
-    
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        self.tabBarItem.image = UIImage(named: "personal")?.resizeImage(scaledTolength: 25)
-        self.tabBarItem.selectedImage = UIImage(named: "personal-selected")?.resizeImage(scaledTolength: 25)
-        self.tabBarItem.imageInsets.top = 5
-        self.tabBarItem.imageInsets.bottom = -5
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
+	let logoutButton = UIButton().then {
+		$0.setTitle("logout", for: .normal)
+		$0.backgroundColor = $0.tintColor
+		$0.layer.cornerRadius = 5
+	}
+	
+	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+		self.tabBarItem.image = UIImage(named: "personal")?.resizeImage(scaledTolength: 25)
+		self.tabBarItem.selectedImage = UIImage(named: "personal-selected")?.resizeImage(scaledTolength: 25)
+		self.tabBarItem.imageInsets.top = 5
+		self.tabBarItem.imageInsets.bottom = -5
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.addSubview(logoutButton)
+		self.logoutButton.snp.makeConstraints { (make) in
+			make.center.equalTo(self.view)
+		}
+	 self.logoutButton.addTarget(self, action: #selector(logoutButtonDidTap), for: .touchUpInside)
+	}
+	
+	func logoutButtonDidTap() {
+		AuthService.instance.logout { (success) in
+			if success {
+				print("haha")
+				NotificationCenter.default.post(name: .presentLogin, object: nil, userInfo: nil)
+			} else {
+				
+			}
+		}
+	}
 }
