732e44fb2f7a4ad00063ea115cb3a900635cb197
torpedo87 <bestofhand@naver.com>
2017-06-20 14:41:26 +0900

    Add postWithsingleImage method

--- Alala/PostEditorViewController.swift
+++ Alala/PostEditorViewController.swift
@@ -9,67 +9,76 @@
 import UIKit
 
 class PostEditorViewController: UIViewController {
-	
-	fileprivate let image: UIImage
-	fileprivate var message: String?
-	fileprivate let tableView = UITableView().then {
-		$0.isScrollEnabled = false
-		$0.register(PostEditorImageCell.self, forCellReuseIdentifier: "imageCell")
-	}
-	
-	init(image: UIImage) {
-		self.image = image
-		super.init(nibName: nil, bundle: nil)
-		self.view.backgroundColor = UIColor.yellow
-		self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share", style: .plain, target: self, action: #selector(shareButtonDidTap))
-	}
-	
-	func shareButtonDidTap() {
-		
-	}
-	
-	required init?(coder aDecoder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		self.tableView.dataSource = self
-		self.tableView.delegate = self
-		
-		self.view.addSubview(self.tableView)
-	}
-	
-	override func viewDidLayoutSubviews() {
-		super.viewDidLayoutSubviews()
-		self.tableView.snp.makeConstraints { make in
-			make.edges.equalTo(self.view)
-		}
-		
-	}
-	
+  
+  fileprivate let image: UIImage
+  fileprivate var message: String?
+  
+  fileprivate let tableView = UITableView().then {
+    $0.isScrollEnabled = false
+    $0.register(PostEditingCell.self, forCellReuseIdentifier: "postEditingCell")
+  }
+  
+  init(image: UIImage) {
+    self.image = image
+    super.init(nibName: nil, bundle: nil)
+    self.view.backgroundColor = UIColor.yellow
+    self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share", style: .plain, target: self, action: #selector(shareButtonDidTap))
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func shareButtonDidTap() {
+    PostService.postWithSingleImage(photo: self.image, message: self.message, progress: nil) { result in
+      print(result)
+    }
+  }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.tableView.dataSource = self
+    self.tableView.delegate = self
+    
+    self.view.addSubview(self.tableView)
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    self.tableView.snp.makeConstraints { make in
+      make.edges.equalTo(self.view)
+    }
+    
+  }
+  
 }
 
 extension PostEditorViewController: UITableViewDataSource, UITextViewDelegate {
-	
-	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 1
-	}
-	
-	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(withIdentifier: "imageCell", for: indexPath) as! PostEditorImageCell
-		cell.configure(image: self.image)
-		
-		return cell
-	}
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "postEditingCell", for: indexPath) as! PostEditingCell
+    cell.imageConfigure(image: self.image)
+    cell.messageConfigure(message: self.message)
+    cell.textDidChange = { [weak self] message in
+      guard let `self` = self else { return }
+      self.message = message
+      self.tableView.beginUpdates()
+      self.tableView.endUpdates()
+      self.tableView.scrollToRow(at: indexPath, at: .none, animated: false)
+    }
+    return cell
+  }
 }
 
 extension PostEditorViewController: UITableViewDelegate {
-	
-	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-		if indexPath.row == 0 { return 100 }
-		return 0
-	}
-	
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    if indexPath.row == 0 { return 100 }
+    return 0
+  }
+  
 }
