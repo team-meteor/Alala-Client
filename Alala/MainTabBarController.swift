e580e57ab83758c5f4315f9a5e51b8375fe26950
torpedo87 <bestofhand@naver.com>
2017-06-22 17:10:05 +0900

    Add tableView

--- Alala/MainTabBarController.swift
+++ Alala/MainTabBarController.swift
@@ -11,57 +11,54 @@ import Photos
 
 class MainTabBarController: UITabBarController {
 
-	let feedViewController = FeedViewController()
-	let discoverViewController = DiscoverViewController()
-	let activityViewController = ActivityViewController()
-	let personalViewController = PersonalViewController()
-    var statusBarShouldBeHidden = false
-	let fakeViewController = UIViewController().then {
-		$0.tabBarItem.image = UIImage(named: "plus")?.resizeImage(scaledTolength: 25)
-		$0.tabBarItem.imageInsets.top = 5
-		$0.tabBarItem.imageInsets.bottom = -5
-	}
-	init() {
-		super.init(nibName: nil, bundle: nil)
-		self.delegate = self
-		self.viewControllers = [
-			UINavigationController(rootViewController: self.feedViewController),
-			self.discoverViewController,
-			fakeViewController,
-			activityViewController,
-			personalViewController
-		]
-	}
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        // Show the status bar
-        statusBarShouldBeHidden = false
-        self.setNeedsStatusBarAppearanceUpdate()
-    }
-    override var prefersStatusBarHidden: Bool {
-        return statusBarShouldBeHidden
-    }
-	required init?(coder aDecoder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
+  let feedViewController = FeedViewController()
+  let discoverViewController = DiscoverViewController()
+  let activityViewController = ActivityViewController()
+  let personalViewController = PersonalViewController()
+  var statusBarShouldBeHidden = false
+  let fakeViewController = UIViewController().then {
+    $0.tabBarItem.image = UIImage(named: "plus")?.resizeImage(scaledTolength: 25)
+    $0.tabBarItem.imageInsets.top = 5
+    $0.tabBarItem.imageInsets.bottom = -5
+  }
+  init() {
+    super.init(nibName: nil, bundle: nil)
+    self.delegate = self
+    self.viewControllers = [
+      UINavigationController(rootViewController: self.feedViewController),
+      self.discoverViewController,
+      fakeViewController,
+      activityViewController,
+      personalViewController
+    ]
+  }
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    // Show the status bar
+    statusBarShouldBeHidden = false
+    self.setNeedsStatusBarAppearanceUpdate()
+  }
+  override var prefersStatusBarHidden: Bool {
+    return statusBarShouldBeHidden
+  }
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 
-	fileprivate func presentWrapperViewController() {
-		let wrapperViewController = WrapperViewController()
-		//self.present(UINavigationController.init(rootViewController: wrapperViewController), animated: true, completion: nil)
-        statusBarShouldBeHidden = true
-        self.setNeedsStatusBarAppearanceUpdate()
-		self.present(wrapperViewController, animated: true, completion: nil)
-	}
+  fileprivate func presentWrapperViewController() {
+    let wrapperViewController = WrapperViewController()
+    self.present(wrapperViewController, animated: true, completion: nil)
+  }
 }
 
 extension MainTabBarController: UITabBarControllerDelegate {
 
-	func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-		if viewController === self.fakeViewController {
-			self.presentWrapperViewController()
-			return false
-		}
-		return true
-	}
+  func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+    if viewController === self.fakeViewController {
+      self.presentWrapperViewController()
+      return false
+    }
+    return true
+  }
 
 }
