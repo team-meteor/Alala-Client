d373e830be927392185146cedbff16920a6dc009
Ellie Kwon <elliebkwon@gmail.com>
2017-06-28 23:40:17 +0900

    Drawing basic UI for personal tab-menu

---
+++ Alala/DiscoverPeopleViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DiscoverPeopleViewController.swift
+//  Alala
+//
+//  Created by Ellie Kwon on 2017. 6. 27..
+//  Copyright © 2017년 team-meteor. All rights reserved.
+//
+
+import Foundation
---
+++ Alala/EditProfileViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EditProfileViewController.swift
+//  Alala
+//
+//  Created by Ellie Kwon on 2017. 6. 28..
+//  Copyright © 2017년 team-meteor. All rights reserved.
+//
+
+import Foundation
