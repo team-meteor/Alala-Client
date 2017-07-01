1370c3100b7eecd3ab2960c5f9cd214c99d7739c
commit <lee@leeui-MacBook-Pro.local>
2017-07-01 18:16:29 +0900

    스크롤뷰 알파값적용

--- Alala/Button.swift
+++ Alala/Button.swift
@@ -16,37 +16,40 @@ class Button: UIButton {
 
     let lineHeight: CGFloat = 1.0
     let lineWidth: CGFloat = min(bounds.width, bounds.height)
-    let linePath = UIBezierPath()
+    let upperLinePath = UIBezierPath()
 
     let upperStartPointX = bounds.width / 2 + bounds.width / 4
     let upperStartPointY = bounds.height / 2 - bounds.height / 4
 
-    linePath.lineWidth = lineHeight
+    upperLinePath.lineWidth = lineHeight
 
-    linePath.move(to: CGPoint(x: upperStartPointX, y: upperStartPointY))
-    linePath.addLine(to: CGPoint(x: upperStartPointX - lineWidth/4, y: upperStartPointY))
-    UIColor.white.setStroke()
-    linePath.stroke()
+    upperLinePath.move(to: CGPoint(x: upperStartPointX, y: upperStartPointY))
+    upperLinePath.addLine(to: CGPoint(x: upperStartPointX - lineWidth/4, y: upperStartPointY))
+    UIColor.white.withAlphaComponent(0.8).setStroke()
+    upperLinePath.stroke()
 
-    linePath.move(to: CGPoint(x: upperStartPointX, y: upperStartPointY))
-    linePath.addLine(to: CGPoint(x: upperStartPointX, y: upperStartPointY + lineWidth/4))
-    UIColor.white.setStroke()
-    linePath.stroke()
+    upperLinePath.move(to: CGPoint(x: upperStartPointX, y: upperStartPointY))
+    upperLinePath.addLine(to: CGPoint(x: upperStartPointX, y: upperStartPointY + lineWidth/4))
+    UIColor.white.withAlphaComponent(0.8).setStroke()
+    upperLinePath.stroke()
 
+    let lowerLinePath = UIBezierPath()
     let lowerStartPointX = bounds.width / 2 - bounds.width / 4
     let lowerStartPointY = bounds.height / 2 + bounds.height / 4
 
-    linePath.lineWidth = lineHeight
+    upperLinePath.lineWidth = lineHeight
 
-    linePath.move(to: CGPoint(x: lowerStartPointX, y: lowerStartPointY))
-    linePath.addLine(to: CGPoint(x: lowerStartPointX + lineWidth/4, y: lowerStartPointY))
-    UIColor.white.setStroke()
-    linePath.stroke()
+    lowerLinePath.lineWidth = lineHeight
 
-    linePath.move(to: CGPoint(x: lowerStartPointX, y: lowerStartPointY))
-    linePath.addLine(to: CGPoint(x: lowerStartPointX, y: lowerStartPointY - lineWidth/4))
-    UIColor.white.setStroke()
-    linePath.stroke()
+    lowerLinePath.move(to: CGPoint(x: lowerStartPointX, y: lowerStartPointY))
+    lowerLinePath.addLine(to: CGPoint(x: lowerStartPointX + lineWidth/4, y: lowerStartPointY))
+    UIColor.white.withAlphaComponent(0.8).setStroke()
+    lowerLinePath.stroke()
+
+    lowerLinePath.move(to: CGPoint(x: lowerStartPointX, y: lowerStartPointY))
+    lowerLinePath.addLine(to: CGPoint(x: lowerStartPointX, y: lowerStartPointY - lineWidth/4))
+    UIColor.white.withAlphaComponent(0.8).setStroke()
+    lowerLinePath.stroke()
 
   }
 
