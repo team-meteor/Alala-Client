ae6c486c6109b44101ec84d2902dd406d6f31040
torpedo87 <bestofhand@naver.com>
2017-07-03 11:01:49 +0900

    multipart service 복구

--- Alala/MultipartService.swift
+++ Alala/MultipartService.swift
@@ -12,25 +12,24 @@ import Alamofire
 struct MultipartService {
   static func uploadMultipart(image: UIImage?, videoData: Data?, progress: Progress?, completion: @escaping (_ multipartId: String) -> Void) {
     let headers = [
-      "Content-Type": "multipart/form-data; charset=utf-8; boundary=__X_PAW_BOUNDARY__",
-      ]
+      "Content-Type": "multipart/form-data; charset=utf-8; boundary=__X_PAW_BOUNDARY__"
+    ]
     Alamofire.upload(multipartFormData: { multipartFormData in
       if videoData != nil {
-
         multipartFormData.append(videoData!, withName: "multipart", fileName: "\(UUID().uuidString).mp4", mimeType: "video/mp4")
       } else {
-
-        let imageData = UIImageJPEGRepresentation(image!, 0.6)!
-        multipartFormData.append(imageData, withName: "multipart", fileName: "\(UUID().uuidString).jpg", mimeType: "image/jpg")
+        if let image = image, let imageData = UIImagePNGRepresentation(image) {
+          multipartFormData.append(imageData, withName: "multipart", fileName: "\(UUID().uuidString).png", mimeType: "image/png")
+        }
       }
     }, to: Constants.BASE_URL + "/multipart", method: .post, headers: headers, encodingCompletion: { result in
       switch result {
       case .success(let upload, _, _):
         upload.responseJSON { response in
           switch response.result {
-          case .success(let data):
-            completion(data as! String)
-
+          case .success(let multipartId):
+            print("multipart it", multipartId)
+            completion(multipartId as! String)
           case .failure(let error):
             print(error)
           }
@@ -39,6 +38,5 @@ struct MultipartService {
         print(encodingError)
       }
     })
-
   }
 }
