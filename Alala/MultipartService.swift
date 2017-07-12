d1f3c1b074bdbeaac86fabc5ffd45c488e75347a
hoemoon <hoemoon.song@gmail.com>
2017-07-12 12:13:38 +0900

    multipart service api modified

--- Alala/MultipartService.swift
+++ Alala/MultipartService.swift
@@ -10,17 +10,15 @@ import UIKit
 import Alamofire
 
 struct MultipartService {
-  static func uploadMultipart(image: UIImage?, videoData: Data?, progress: Progress?, completion: @escaping (_ multipartId: String) -> Void) {
+  static func uploadMultipart(multiPartData: Any, progress: Progress?, completion: @escaping (_ multipartId: String) -> Void) {
     let headers = [
       "Content-Type": "multipart/form-data; charset=utf-8; boundary=__X_PAW_BOUNDARY__"
     ]
     Alamofire.upload(multipartFormData: { multipartFormData in
-      if videoData != nil {
-        multipartFormData.append(videoData!, withName: "multipart", fileName: "\(UUID().uuidString).mp4", mimeType: "video/mp4")
-      } else {
-        if let image = image, let imageData = UIImagePNGRepresentation(image) {
-          multipartFormData.append(imageData, withName: "multipart", fileName: "\(UUID().uuidString).png", mimeType: "image/png")
-        }
+      if let videoData = multiPartData as? Data {
+        multipartFormData.append(videoData, withName: "multipart", fileName: "\(UUID().uuidString).mp4", mimeType: "video/mp4")
+      } else if let image = multiPartData as? UIImage, let imageData = UIImagePNGRepresentation(image) {
+        multipartFormData.append(imageData, withName: "multipart", fileName: "\(UUID().uuidString).png", mimeType: "image/png")
       }
     }, to: Constants.BASE_URL + "/multipart", method: .post, headers: headers, encodingCompletion: { result in
       switch result {
