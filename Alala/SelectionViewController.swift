580004c03760fa424a074b061be04c88de5b3dc2
commit <lee@leeui-MacBook-Pro.local>
2017-07-12 16:48:51 +0900

    first commit

--- Alala/SelectionViewController.swift
+++ Alala/SelectionViewController.swift
@@ -605,10 +605,6 @@ extension SelectionViewController: UIScrollViewDelegate {
   }
   func scrollViewDidScroll(_ scrollView: UIScrollView) {
 
-//    if scrollView.contentOffset.x > scrollView.contentSize.width - scrollView.bounds.width {
-//      scrollView.contentOffset.x = scrollView.contentSize.width - scrollView.bounds.width
-//    }
-
     let page = self.baseScrollView.contentOffset.y
 
     if page >= 44 {
@@ -619,12 +615,13 @@ extension SelectionViewController: UIScrollViewDelegate {
     }
     self.cropAreaView.backgroundColor = UIColor.black.withAlphaComponent(page / 600)
   }
+  
   func scrollViewDidZoom(_ scrollView: UIScrollView) {
 
     let imageViewSize = imageView.frame.size
     let scrollViewSize = scrollView.bounds.size
 
-    let verticalPadding = imageViewSize.height < scrollViewSize.height ?  (scrollViewSize.height - imageViewSize.height) / 2 : 0
+    let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
     let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
 
     scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
