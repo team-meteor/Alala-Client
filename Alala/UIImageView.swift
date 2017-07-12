c4f4d67349adc2ea3ddc02b6ad8c19f82f01cb7a
torpedo87 <bestofhand@naver.com>
2017-07-12 12:18:04 +0900

    임시저장

--- Alala/UIImageView.swift
+++ Alala/UIImageView.swift
@@ -20,9 +20,9 @@ extension UIImageView {
     DispatchQueue.main.async {
       self.kf.setImage(with: url, placeholder: placeholder)
     }
-
+    
   }
-
+  
   func setVideo(videoId: String, completion: @escaping (_ success: Bool) -> Void) {
     let url = URL(string: "https://s3.ap-northeast-2.amazonaws.com/alala-static/\(videoId)")
     let playerItem = AVPlayerItem(url: url!)
@@ -33,9 +33,27 @@ extension UIImageView {
       playerLayer.frame = self.frame
       print("playerframe", playerLayer.frame)
     }
-
+    
     player.play()
     completion(true)
   }
-
+  
+  func imageFrame() -> CGRect {
+    let imageViewSize = self.frame.size
+    guard let imageSize = self.image?.size else{return CGRect.zero}
+    let imageRatio = imageSize.width / imageSize.height
+    let imageViewRatio = imageViewSize.width / imageViewSize.height
+    if imageRatio < imageViewRatio {
+      let scaleFactor = imageViewSize.height / imageSize.height
+      let width = imageSize.width * scaleFactor
+      let topLeftX = (imageViewSize.width - width) * 0.5
+      return CGRect(x: topLeftX, y: 0, width: width, height: imageViewSize.height)
+    }else{
+      let scalFactor = imageViewSize.width / imageSize.width
+      let height = imageSize.height * scalFactor
+      let topLeftY = (imageViewSize.height - height) * 0.5
+      return CGRect(x: 0, y: topLeftY, width: imageViewSize.width, height: height)
+    }
+  }
+  
 }
